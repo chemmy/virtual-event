66e4978ce3273018992f3a875e9270a397923aef
Chemmy Tutor <chemmy0430@gmail.com>
2022-10-14 06:05:17 +0800

    Add font and display correct name on welcome

--- src/app/modules/welcome-page/components/welcome/welcome.component.ts
+++ src/app/modules/welcome-page/components/welcome/welcome.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AppState } from 'src/app/state/app.state';
+import { selectUserDetails } from 'src/app/state/user/user.selectors';
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  public userName: string = '';
+  private subscription!: Subscription;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.subscription = this.store.select(selectUserDetails).subscribe((data) => {
+      this.userName = data.name;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
